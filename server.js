3a8c3e2c7697ca20a5654f8e4b1b5c13c01a8a33
EricKim86 <113047098+EricKim86@users.noreply.github.com>
2022-12-09 17:06:08 -0500

    set up dependencies and server

--- server.js
+++ server.js
@@ -1,9 +1,14 @@
-const express = require('express');
-const app = express();
+const express = require("express");
+const path = require("path");
+const fs = require("fs");
+const util = require("util");
+
 const PORT = 3001;
+const app = express();
 
-app.use(express.static('public'));
+app.use(express.static("public"));
 
-app.listen(PORT, () => {
-    console.log(`http://localhost:${PORT}`);
-  })
+
+app.listen(PORT, () =>
+  console.log(`Express server listening on port ${PORT}!`)
+);
