c192300a86c242c7b1301d0d7e536530fa5a3383
EricKim86 <113047098+EricKim86@users.noreply.github.com>
2022-12-12 15:13:57 -0500

    added read/write code to allow user to input notes

--- server.js
+++ server.js
@@ -1,12 +1,10 @@
 const express = require("express");
 const path = require("path");
 const fs = require("fs");
-const util = require("util");
 
 const PORT = 3001;
 const app = express();
 const uuid = require('./helpers/uuid');
-
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
@@ -22,73 +20,55 @@ app.get('/db', (req, res) =>
   res.sendFile(path.join(__dirname, '/db/db'))
 );
 
-// Promise version of fs.readFile
-const readFromFile = util.promisify(fs.readFile);
-
-/**
- *  Function to write data to the JSON file given a destination and some content
- *  @param {string} destination The file you want to write to.
- *  @param {object} content The content you want to write to the file.
- *  @returns {void} Nothing
- */
-const writeToFile = (destination, content) =>
-  fs.writeFile(destination, JSON.stringify(content, null, 4), (err) =>
-    err ? console.error(err) : console.info(`\nData written to ${destination}`)
-  );
-
-/**
- *  Function to read data from a given a file and append some content
- *  @param {object} content The content you want to append to the file.
- *  @param {string} file The path to the file you want to save to.
- *  @returns {void} Nothing
- */
-const readAndAppend = (content, file) => {
-  fs.readFile(file, 'utf8', (err, data) => {
-    if (err) {
-      console.error(err);
-    } else {
-      const parsedData = JSON.parse(data);
-      parsedData.push(content);
-      writeToFile(file, parsedData);
-    }
-  });
-};
-
-
-// GET Route for retrieving all the feedback
+// GET request for note
 app.get('/api/db', (req, res) => {
-  console.info(`${req.method} request received for Notes`);
 
-  readFromFile('./db/db.json').then((data) => res.json(JSON.parse(data)));
+// Send a message to the client
+  res.json(`${req.method} request received to get notes`);
+  console.info(`${req.method} request received to get notes`);
 });
 
-// POST Route for submitting feedback
+// POST request to add a note
 app.post('/api/db', (req, res) => {
-  // Log that a POST request was received
-  console.info(`${req.method} request received to submit Notes`);
+
+  console.info(`${req.method} request received to add a notes`);
 
   // Destructuring assignment for the items in req.body
   const { title, text } = req.body;
 
-  // If all the required properties are present
   if (title && text) {
-    // Variable for the object we will save
     const newNote = {
       title,
       text,
       note_id: uuid(),
     };
 
-    readAndAppend(newNote, './db/db.json');
+    fs.readFile('./db/db.json', `utf-8`, (err, data) => {
+      if (err) {
+        console.log(err);
+      } else {
+        const notes = JSON.parse(data);
+        notes.push(newNote);
+
+        fs.writeFile(`./db/db.json`, JSON.stringify(notes, null, `\t`), (err) =>
+          err
+            ? console.error(err)
+            : console.log(
+              `Review for ${newNote.title} has been written to JSON file`
+            )
+        );
+      }
+    })
 
     const response = {
       status: 'success',
-      body: newFeedback,
+      body: newNote,
     };
 
-    res.json(response);
+    console.log(response);
+    res.status(201).json(response);
   } else {
-    res.json('Error in posting feedback');
+    res.status(500).json('Error in posting note');
   }
 });
 
